d2184c989abc14a791b87614e9a5a2e3ab592e67
LuizGarbini <garbasneto@gmail.com>
2024-01-22 22:36:44 -0300

    finish lesson 9

--- scripts.js
+++ scripts.js
@@ -1,46 +1,48 @@
 /* 
-  Crie uma lista de pacientes
+  Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
 
-  Cada paciente dentro da lista, deverá conter
-    nome
-    idade
-    peso
-    altura
+  "Paciente X possui o IMC de: Y"
 
-  Escreva uma lista contendo o nome dos pacientes
+  Onde X é o nome do paciente e Y é o IMC desse paciente
+
+  Crie uma função para fazer o cálculo de IMC
 */
 
-const patients =
-[ {
-    name: "Luiz",
+/* peso / (altura * altura) */
+
+const patients = [
+  {
+    name: 'Luiz',
     age: 21,
     weight: 72,
-    height: 180
-},
-{
-    name: "Samanta",
+    height: 180,
+  },
+  {
+    name: 'Samanta',
     age: 20,
     weight: 45,
-    height: 165
-},
-{
-    name: "Luiza",
+    height: 165,
+  },
+  {
+    name: 'Luiza',
     age: 12,
     weight: 60,
-    height: 160
-}
-]
+    height: 160,
+  },
+];
 
-let patientsNames = []
-let patientsAges = []
-let patientsWeights = []
-let patientsHeights = []
-
-for(let patient of patients) {
-    patientsNames.push(patient.name)
-    patientsAges.push(patient.age)
-    patientsWeights.push(patient.weight)
-    patientsHeights.push(patient.height)
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2);
 }
 
-alert(`patientsNames`)
+function printPatientIMC(patient) {
+  return `Paciente ${patient.name} possui o IMC de: ${IMC(
+    patient.weight,
+    patient.height
+  )}`;
+}
+
+for (let patient of patients) {
+  let IMCMessage = printPatientIMC(patient);
+  alert(IMCMessage);
+}
